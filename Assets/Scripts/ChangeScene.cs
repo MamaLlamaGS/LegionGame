93f6b4741a02c492549e1d51784899b49b2286d1
Sandee Curtis <sandee4746@gmail.com>
2018-03-17 20:43:00 +0000

    the rest of the updated files

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	Scene titleScreen;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Starting ChangeScene");
+		titleScreen = SceneManager.GetActiveScene ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ManageLoadingInitialScene() {
+		// wait 3 seconds or move to main menu if touched
+		while (Time.realtimeSinceStartup < 3)
+		{
+			if (Input.touchSupported )
+			{
+				if (Input.GetTouch(0).tapCount > 0)
+				{
+					Debug.Log ("Touched: Changing to main screen");
+					SceneManager.LoadScene ("Main Screen");
+				}
+			}
+			else {
+				if (Input.GetKeyDown ("space")) {
+					Debug.Log ("Spaced: Changing to main screen");
+					SceneManager.LoadScene ("Main Screen");
+					//Application.LoadLevel("Title Screen");
+				}
+			}
+		}
+		if (SceneManager.GetActiveScene() == titleScreen) {
+			Debug.Log ("Timed out: Changing to main screen");
+			SceneManager.LoadScene ("Main Screen");
+		}
+	}
+}
