82b8de6c805b126bb79c6bef87b8042e6e5f8d1d
Mama Llama <mamallamagamestudio@gmail.com>
2018-03-29 10:28:52 -0700

    Color changing system started in main game scene and buttons created in main menu and level scene

---
+++ Assets/Scripts/ChangingColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingColor : MonoBehaviour
+{
+    public GameObject panel;
+
+    
+
+    
+        
+    
+    public void OpenPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        panel.SetActive(false);
+    }
+}
+
+	
