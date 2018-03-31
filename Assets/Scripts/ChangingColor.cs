5d8932d834ea7580e4943b4259432ee35ea06ed7
Sandee Curtis <sandee4746@gmail.com>
2018-03-31 17:17:49 +0100

    updated changing color so that panels opens and closes

--- Assets/Scripts/ChangingColor.cs
+++ Assets/Scripts/ChangingColor.cs
@@ -9,15 +9,28 @@ public class ChangingColor : MonoBehaviour
 
     public SpriteRenderer head;
     public SpriteRenderer body;
-    public Image squareHeadDisplay; //creation and connection of buttons to color
+    //public Image squareHeadDisplay; //creation and connection of buttons to color
 
-    public Color[] colors;
+	public Color[] colors;
     
     public int whatColor = 1;
 
+	public void OpenPanel() {
+		Debug.Log ("ChangingColor: OpenPanel");
+
+		panel.SetActive (true);
+
+	}
+
+	public void ClosePanel() {
+		Debug.Log ("ChangingColor: ClosePanel");
+		panel.SetActive (false);
+
+	}
+
     private void Update()
     {
-        squareHeadDisplay.color = head.color; //creation and connection of buttons to color
+		//squareHeadDisplay.color = head.color; //creation and connection of buttons to color
 
         for (int i= 0; i < colors.Length; i++)
         {
@@ -26,16 +39,17 @@ public class ChangingColor : MonoBehaviour
                 body.color = colors[i];//connection to both body and face?
             }
         }
-        
     }
+		
 
    public void ChangePanelState (bool state)
     {
-        panel.SetActive(true);//state issue?
+        panel.SetActive(true); // whether panel is visible
     }
 
-    public void changeHeadColor(int index)
+    public void changeDemonColor(int index)
     {
+		Debug.Log ("ChangeColor: changeDemonColor and index is " + index);
         whatColor = index;
     }
 }
