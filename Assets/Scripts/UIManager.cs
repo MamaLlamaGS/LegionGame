43f4d501b053e5b79137c4f118a3482a1d19ab55
Mama Llama <mamallamagamestudio@gmail.com>
2018-04-03 17:55:42 -0700

    more too

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
+    public RectTransfrom btnprefab;
+    private FeatureManager mgr;
+    private Text descText;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        mgr = FindObjectofType<FeatureManager>();
+        descText = transform.FindChild("Navigation").FindChild("Text").GetComponent<descText>();
+        transform.FindChild("Navigation").FindChild("Previous").GetCompentent<Button>().onClick.AddListener(() => mgr.PreviousChoice());
+        transform.FindChild("Navigation").FindChild("Previous").GetCompentent<Button>().onClick.AddListener(() => mgr.NextChoice());
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        descText.Text = mgr.feature[mgr.currfeature].ID + " #" + (mgr.features[mgr.currFeature]).currIndex + 1).ToString();
+
+
+    }
 }
