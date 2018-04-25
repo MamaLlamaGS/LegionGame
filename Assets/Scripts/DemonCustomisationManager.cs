76f232088c12a8a4d31dba0b454b37c1184a4b75
Sandee Curtis <sandee4746@gmail.com>
2018-04-25 20:18:51 +0100

    changes to navigation and scripts

---
+++ Assets/Scripts/DemonCustomisationManager.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+// makes this run even when not playing the game
+[ExecuteInEditMode]
+public class DemonCustomisationManager : MonoBehaviour
+{
+
+    // this class comes from https://www.youtube.com/watch?v=xoIagG1RVeE
+
+    // all the sprites need to be named consecutively (ie, face_0, face_1)
+
+    public List <Feature> features;
+    public int currentFeature;
+    private GameObject face;
+
+    void OnEnable()
+    {
+        LoadFeatures();
+    }
+
+    void OnDisable()
+    {
+        SaveFeatures();
+    }
+
+    // get the group of features from the assets themselves
+    void LoadFeatures()
+    {
+        features = new List <Feature>();
+        face = GameObject.Find("Demon/Face");
+        //Debug.Log("LoadFeatures: face is " + face.name);
+        features.Add(new Feature("Ears", face.transform.Find("Ears").GetComponent<SpriteRenderer>()));
+        //Debug.Log("LoadFeatures: Ears is " + face.transform.Find("Ears").name);
+        features.Add(new Feature("Eyes", face.transform.Find("Eyes").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Nose", face.transform.Find("Nose").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Mouth", face.transform.Find("Mouth").GetComponent<SpriteRenderer>()));
+        //features.Add(new Feature("ArmsWings", transform.Find("ArmsWings").GetComponent<SpriteRenderer>()));
+        //features.Add(new Feature("Feet", transform.Find("Feet").GetComponent<SpriteRenderer>()));
+        //features.Add(new Feature("Body", transform.Find("Body").GetComponent<SpriteRenderer>()));
+
+
+        if (features.Count != null) {
+            for (int i = 0; i < features.Count; i++)
+            {
+                string key = "FEATURE_" + i;
+                if (!PlayerPrefs.HasKey(key))
+                    PlayerPrefs.SetInt(key, features[i].currentIndex);
+                features[i].currentIndex = PlayerPrefs.GetInt(key);
+                //Debug.Log("LoadFeatures: feature key is " + key);
+                features[i].UpdateFeature();
+            }
+          
+        }
+    }
+
+    void SaveFeatures()
+    {
+        for (int i = 0; i < features.Count; i++)
+        {
+            string key = "FEATURE_" + i;
+            PlayerPrefs.SetInt(key, features[i].currentIndex);
+        }
+        PlayerPrefs.Save();
+    }
+
+    // set the feature type based on the feature icon button
+    public void SetCurrentFeatureType(int index)
+    {
+        if (features == null)
+        {
+            return;
+        }
+        else
+        {
+            currentFeature = index;
+        }
+    }
+/*
+    // reaction to next feature button
+    public void NextChoice(int index)
+    {
+        if (features == null)
+            return;
+        features[currentFeature].currentIndex++;
+        features[currentFeature].UpdateFeature();
+        //Debug.Log("NextChoice: index is " + index);
+  }
+*/
+    // overload method with 0 arguments for adding listener
+    public void NextChoice()
+    {
+        if (features == null)
+            return;
+        features[currentFeature].currentIndex++;
+        features[currentFeature].UpdateFeature();
+        //Debug.Log("NextChoice: (0): inside");
+    }
+
+    // reaction to previous feature button
+/*    public void PrevChoice(int index)
+    {
+        if (features == null)
+            return;
+        features[currentFeature].currentIndex--;
+        features[currentFeature].UpdateFeature();
+        //Debug.Log("PrevChoice: index is " + index);
+    }
+*/
+    // overload method with 0 arguments for adding listener
+    public void PrevChoice()
+    {
+        if (features == null)
+            return;
+        features[currentFeature].currentIndex--;
+        features[currentFeature].UpdateFeature();
+        //Debug.Log("PrevChoice(0): inside");
+    }
+
+
+
+
+    // makes it so it can be seen in the Inspector
+    [System.Serializable]
+    // Feature class within
+    public class Feature
+    {
+        public string ID;
+        public int currentIndex;
+        public Sprite[] choices;
+        public SpriteRenderer renderer;
+
+        public Feature(string id, SpriteRenderer rend)
+        {
+            ID = id;
+            renderer = rend;
+            UpdateFeature();
+        }
+
+
+        public void UpdateFeature()
+        {
+            // goes to get the list of choices from what is available from the Assets/Sprites folder
+            choices = Resources.LoadAll<Sprite>("DemonParts/" + ID);
+            if (choices == null || renderer == null)
+            {
+                return;
+            }
+            if (currentIndex < 0)
+            {
+                currentIndex = choices.Length - 1;
+            }
+
+            if (currentIndex >= choices.Length)
+            {
+                currentIndex = 0;
+            }
+
+            renderer.sprite = choices[currentIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/DemonUIManager.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DemonUIManager : MonoBehaviour
+{
+
+    private DemonCustomisationManager manager;
+    private Text descText;
+    private Button eyesButton;
+    private Button earsButton;
+    private Button noseButton;
+    private Button mouthButton;
+    private GameObject bottomNavigation = null;
+    private int featureIndex;
+    //private Button nextButton;
+    //private Button previousButton;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        manager = FindObjectOfType<DemonCustomisationManager>();
+        bottomNavigation = GameObject.Find("Navigation");
+        descText = bottomNavigation.transform.Find("Description").GetComponent<Text>();
+
+        bottomNavigation.transform.Find("Previous").GetComponent<Button>().onClick.AddListener(() => manager.PrevChoice());
+        bottomNavigation.transform.Find("Next").GetComponent<Button>().onClick.AddListener(() => manager.NextChoice());
+        earsButton = GameObject.Find("Ears Button").GetComponent<Button>(); 
+        earsButton.onClick.AddListener(() => manager.SetCurrentFeatureType(GetFeatureIndex(earsButton)));
+        eyesButton = GameObject.Find("Eyes Button").GetComponent<Button>();
+        eyesButton.onClick.AddListener(() => manager.SetCurrentFeatureType(GetFeatureIndex(eyesButton)));
+        noseButton = GameObject.Find("Nose Button").GetComponent<Button>();
+        noseButton.onClick.AddListener(() => manager.SetCurrentFeatureType(GetFeatureIndex(noseButton)));
+        mouthButton = GameObject.Find("Mouth Button").GetComponent<Button>();
+        mouthButton.onClick.AddListener(() => manager.SetCurrentFeatureType(GetFeatureIndex(mouthButton)));
+        //InitializeFeatureButtons();
+    }
+
+
+    private int GetFeatureIndex (Button b) {
+        if (b.name == "Ears Button") {
+            return 0;
+        } else if (b.name == "Eyes Button") {
+            return 2;
+           
+        } else if (b.name == "Nose Button") {
+            return 2;
+        }
+        else 
+        {
+            return 3;
+        }
+    }
+
+    private Button GetButtonByIndex (int i) {
+        if (i == 0)
+        {
+            return earsButton;
+        }
+        else if (i == 1)
+        {
+            return eyesButton;
+
+        }
+        else if (i == 2)
+        {
+            return noseButton;
+        }
+        else
+        {
+            return mouthButton;
+        }
+       
+    }
+
+    /*
+    private void InitializeFeatureButtons()
+    {
+        buttons = new List<Button>();
+
+        float height = ears.rect.height;
+        float width = ears.rect.width;
+
+        for (int i = 0; i < manager.features.Count; i++)
+        {
+            RectTransform temp = Instantiate<RectTransform>(ears);
+            temp.name = i.ToString();
+            temp.SetParent(transform.Find("Features").GetComponent<RectTransform>());
+            temp.localScale = new Vector3(1, 1, 1);
+            temp.localPosition = new Vector3(0, 0, 0);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, width);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, i * height, height);
+
+            Button b = temp.GetComponent<Button>();
+            b.onClick.AddListener(() => manager.SetCurrent(int.Parse(temp.name)));
+            buttons.Add(b);
+        } 
+    }
+
+    void UpdateFeatureButtons()
+    {
+        for (int i = 0; i < manager.features.Count; i++)
+        {
+            // first get the feature name and get the image that matches
+            string featureName = manager.features[i].ID;
+            Debug.Log("UpdateFeatureButtons: featureName is " + featureName);
+            //buttons[i].transform.Find(featureName).GetComponent<Image>().sprite = manager.features[i].renderer.sprite;
+            buttons[i].transform.Find("earicondial").GetComponent<Image>().sprite = manager.features[i].renderer.sprite;
+        } 
+    } */
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UpdateFeatureButtons();
+        EventSystem.current.SetSelectedGameObject(GetButtonByIndex(manager.currentFeature).gameObject);
+        descText.text = manager.features[manager.currentFeature].ID + " #" + (manager.features[manager.currentFeature].currentIndex + 1).ToString();
+        Debug.Log("Update: descText is " + descText.text);
+    }
+
+}
